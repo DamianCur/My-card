1cb808ec4b5e54f54e2620b47be3087d79e243ef
Damian Cur <damian.cur91@gmail.com>
2020-10-20 16:47:29 +0200

    fixing flex (in progress)

--- js/script.js
+++
@@ -1,5 +0,0 @@
-let js__secondProject = document.querySelector("secoundPage");
-
-js__secondProject.addEventListener("click", function(){
-    document.location.href = 'https://codepen.io/Damian_Cur/full/zYrKVXb';
-    });
