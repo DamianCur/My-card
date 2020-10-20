f3718b8b9916d036e2df66115fc457e486b7289d
Damian Cur <damian.cur91@gmail.com>
2020-10-20 15:42:42 +0200

    centred gifs with links

---
+++ js/script.js
@@ -0,0 +1,5 @@
+let js__secondProject = document.querySelector("secoundPage");
+
+js__secondProject.addEventListener("click", function(){
+    document.location.href = 'https://codepen.io/Damian_Cur/full/zYrKVXb';
+    });
