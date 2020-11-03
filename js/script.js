0d64bf8ec723aff956a039a393d8f0c214d6baf5
Damian Cur <damian.cur91@hmail.com>
2020-11-03 20:05:30 +0100

    changed background color

--- js/script.js
+++ js/script.js
@@ -2,7 +2,9 @@
 
 let headerButton = document.querySelector(".header__button");
 let body = document.querySelector(".body");
+let article = document.querySelector("article");
 
 headerButton.addEventListener("click", () => {
     body.classList.toggle("js__body");
 });
+
